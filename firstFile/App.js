268f5cab61e38d5bc320a319390814359bfc1091
YojanaRegmi <yojuyoji@gmail.com>
2020-03-24 10:40:20 +0000

    button and list added

--- firstFile/App.js
+++ firstFile/App.js
@@ -1,19 +1,44 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+  import React,{useState} from 'react';
+  import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+  export default function App() {
+    const [anything, setAnything]=useState('');
+    const [arrayData, setArrayData]=useState([]);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    const addValue= (enteredText)=>{
+      setAnything(enteredText);
+    }
+
+    const saveData=()=>{
+      setArrayData(currentData=>[...currentData, anything]);
+    }
+
+    return (
+      <View style={styles.container}>
+
+        <View>
+          <TextInput 
+          placeholder='enter anything to be displayed'
+          onChangeText={addValue}
+          />
+          <Button title='add' onPress={saveData}/>
+          </View>
+
+        <View>
+         <Text>Open up App.js to start working on your app!</Text>
+         <Text>display is printed here</Text>
+         <Text>{arrayData.map((data) => <Text>{data}</Text>)}</Text>
+        </View>
+
+      </View>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
